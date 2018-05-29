7c42d41f82e2445e76f7c637c5dff6b4ad2b4574
Jalmari Talvipuro <jalmari.talvipuro@stud.pointcollege.fi>
2018-05-29 14:30:37 +0300

    tehty lämpötilan tallennus

--- tiedontallennus/tiedontallennus/Program.cs
+++ tiedontallennus/tiedontallennus/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,17 @@ namespace tiedontallennus
     {
         static void Main(string[] args)
         {
+            int lämpötila = 0;
+            Console.Write("Anna uusi lämpötila: ");
+            string syöte =Console.ReadLine();
+            lämpötila = int.Parse(syöte);
+
+            //tiedoston kirjoittaminen
+            string tiedosto = @"C:\test\lämpötila.txt";
+            File.WriteAllText(tiedosto, lämpötila.ToString());
+            Console.WriteLine("Uusi lämpötila tallennettu");
+
+            Console.ReadLine();
         }
     }
 }
